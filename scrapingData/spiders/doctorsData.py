850570a15c67ebd00664cb38bbd7d4756e40010f
NikosSpyropoulos <nikspyropoylos@gmail.com>
2021-03-15 00:30:26 +0200

    Extracted the desired data, but there some issues Issues: in the .csv there are gaps and empty lines between the data ToDo: change the code by using items.py, extract data from more pages(now only from the 1st), fix the issue

--- scrapingData/spiders/doctorsData.py
+++ scrapingData/spiders/doctorsData.py
@@ -9,9 +9,18 @@ class DoctorsDataSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        for advCategory in response.xpath("//div[@class = 'AdvAreaLeft']//a"):
+        for advCategory in response.xpath("//div[@class = 'AdvAreaLeft']/div"):
             yield {
-                'Office Name': advCategory.xpath("meta/@content").extract_first()
+                'Office Name': advCategory.xpath("div/div/h2/a/meta/@content".strip()).extract_first(),
+                'Description': advCategory.xpath("div/div/h2/meta/@content".strip()).extract_first(),
+                'Category': advCategory.xpath("div[3]/div/div[3]/div/span/text()".strip()).extract_first(),
+                'Address': advCategory.xpath("div[3]/div/div[2]/text()".strip()).extract_first(),
+                'Office Phone': advCategory.xpath("div[4]/div[3]/div[2]/text()".strip()).extract_first(),
+                'Office Phone': advCategory.xpath("div[4]/div[3]/div[2]/text()".strip()).extract_first(),
+                'Mobile Phone': advCategory.xpath("div[4]/div[3]/div[5]/text()".strip()).extract_first(),
+                'Email': advCategory.xpath("div[4]/div[3]/div[8]/a[contains(@href, 'mailto')]/@href".strip()).extract_first(),
+                'Website': advCategory.xpath("div[4]/div[3]/div[11]/a/text()".strip()).extract_first()
+
             }
 
         # for advCategory in response.xpath("//div[@class='PhonesBox']"):
